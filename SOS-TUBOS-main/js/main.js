1468f4acabd3e7fddc6213e3a6216b5193977376
Nicolas Fernandes <146742635+fernandesnic@users.noreply.github.com>
2025-06-09 15:14:49 -0300

    Delete SOS-TUBOS-main directory

--- SOS-TUBOS-main/js/main.js
+++
@@ -1,7 +0,0 @@
-const menuToggle = document.querySelector('.menu-toggle');
-const navMenu = document.querySelector('.nav-menu');
-
-menuToggle.addEventListener('click', () => {
-  navMenu.classList.toggle('ativo');
-  menuToggle.classList.toggle('open');
-});
